6f1b12c0230ff4937a5d1a7ceb7d3694a46c02cb
Tibo <Tibowl@users.noreply.github.com>
2019-09-20 23:51:27 +0200

    Use embeds for long tweets

--- src/utils/TweetManager.js
+++ src/utils/TweetManager.js
@@ -1,4 +1,6 @@
 const Twit = require('twit')
+const Discord = require('discord.js');
+
 exports.client = undefined;
 exports.stream = undefined;
 
@@ -11,11 +13,37 @@ exports.init = (client) => {
     this.stream.on('tweet', (tweet) => {
         if(!toFollow.includes(tweet.user.id_str)) return;
 
-        const tweetLink =` https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+        const tweetLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
         console.log(`Sending tweet to channels: ${tweetLink}`)
 
+        if(tweet.retweeted_status)
+            tweet = tweet.retweeted_status
+            
+        // Tweet has media, don't embed it
+        if(tweet.extended_entities || (tweet.extended_tweet && tweet.extended_tweet.extended_entities)) {
+            for(let channel of client.config.tweetChannels)
+                this.client.channels.get(channel).send(tweetLink)
+            return;
+        }
+
+        let text = tweet.text;
+        
+        if(tweet.extended_tweet) {
+            text = tweet.extended_tweet.full_text
+            for(const url of tweet.extended_tweet.entities.urls) 
+                text = text.replace(url.url, url.expanded_url)
+        } else {
+            for(const url of tweet.entities.urls) 
+                text = text.replace(url.url, url.expanded_url)
+        }
+
+        const embed = new Discord.RichEmbed()
+            .setAuthor(tweet.user.name, tweet.user.profile_image_url_https, `https://twitter.com/${tweet.user.screen_name}`)
+            .setDescription(text)
+            .setColor(`#${tweet.user.profile_background_color}`)
+
         for(let channel of client.config.tweetChannels)
-            this.client.channels.get(channel).send(tweetLink)
+            this.client.channels.get(channel).send(`<${tweetLink}>`, embed)
     })
     console.log(`Following ${toFollow.length} twitter account(s)!`)
 }
