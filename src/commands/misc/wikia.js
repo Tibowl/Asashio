4aa50d123b0b656f6ad055f39e73fffdb9fb4e97
Tibo <Tibowl@users.noreply.github.com>
2019-09-22 12:46:23 +0200

    Fix empty wikia response

--- src/commands/misc/wikia.js
+++ src/commands/misc/wikia.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 exports.run = async (client, message, args) => {
     if(!args || args.length < 1) return message.channel.send(`<http://kancolle.fandom.com/>`)
     const result = await (await fetch(`https://kancolle.fandom.com/api/v1/Search/List?query=${encodeURIComponent(args.join(" "))}&limit=5&namespaces=0,14`)).json()
-    if(result.items.length == 0)
+    if(!result.items || result.items.length == 0)
         return message.channel.send(`No matches found`);
     return message.channel.send(`Possible matches:\n${result.items.map(i => `<${i.url}>`).join("\n")}`);
 }
