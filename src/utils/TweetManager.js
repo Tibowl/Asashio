1b0040b89784678d64e81a67c4f7fa13d56065a3
Tibo <Tibowl@users.noreply.github.com>
2019-09-27 15:10:46 +0200

    Improve tweet handling

--- src/utils/TweetManager.js
+++ src/utils/TweetManager.js
@@ -9,56 +9,63 @@ exports.init = (client) => {
     this.client = client
     const T = new Twit(client.config.twitter)
 
-    const toFollow = client.config.toTweet
-    this.stream = T.stream("statuses/filter", { follow: toFollow})
-    this.stream.on("tweet", (tweet) => {
-        if(!toFollow.includes(tweet.user.id_str)) return
-        const tweetLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-        if(tweet.retweeted_status) return console.log(`Skipping RT ${tweetLink}`)
+    this.toFollow = client.config.toTweet
+    this.stream = T.stream("statuses/filter", { follow: this.toFollow })
+    this.stream.on("tweet", this.handleTweet)
 
-        // @KCServerWatcher
-        if(tweet.user.id_str == "980204936687489025") {
-            let text = tweet.text
+    console.log(`Following ${this.toFollow.length} twitter account(s)!`)
+}
 
-            if(tweet.extended_tweet)
-                text = tweet.extended_tweet.full_text
+exports.handleTweet = (tweet) => {
+    if(!this.toFollow.includes(tweet.user.id_str)) return
+    const tweetLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+    if(tweet.retweeted_status && this.toFollow.includes(tweet.retweeted_status.user.id_str)) return console.log(`Skipping RT ${tweetLink}`)
 
-            if(text.includes("Game version") || text.includes("Maintenance ended") || text.includes("Maintenance ongoing"))
-                Utils.sendToChannels(client, client.config.tweetChannels, text)
-
-            return
-        }
-
-        console.log(`Sending tweet to channels: ${tweetLink}`)
-
-        if(tweet.retweeted_status)
-            tweet = tweet.retweeted_status
-
-        // Tweet has media, don't embed it
-        if(tweet.extended_entities || (tweet.extended_tweet && tweet.extended_tweet.extended_entities)) {
-            Utils.sendToChannels(client, client.config.tweetChannels, tweetLink)
-            return
-        }
-
+    // @KCServerWatcher
+    if(tweet.user.id_str == "980204936687489025") {
         let text = tweet.text
 
-        if(tweet.extended_tweet) {
+        if(tweet.extended_tweet)
             text = tweet.extended_tweet.full_text
-            for(const url of tweet.extended_tweet.entities.urls)
-                text = text.replace(url.url, url.expanded_url)
-        } else {
-            for(const url of tweet.entities.urls)
-                text = text.replace(url.url, url.expanded_url)
-        }
 
-        const embed = new Discord.RichEmbed()
-            .setAuthor(tweet.user.name, tweet.user.profile_image_url_https, `https://twitter.com/${tweet.user.screen_name}`)
-            .setDescription(text)
-            .setColor(`#${tweet.user.profile_background_color}`)
+        if(text.includes("Game version") || text.includes("Maintenance ended") || text.includes("Maintenance ongoing"))
+            Utils.sendToChannels(this.client, this.client.config.tweetChannels, text)
 
-        Utils.sendToChannels(client, client.config.tweetChannels, `<${tweetLink}>`, embed)
-    })
-    console.log(`Following ${toFollow.length} twitter account(s)!`)
+        return
+    }
+
+    console.log(`Sending tweet to channels: ${tweetLink}`)
+
+    if(tweet.retweeted_status)
+        tweet = tweet.retweeted_status
+
+    const embed = new Discord.RichEmbed()
+        .setAuthor(tweet.user.name, tweet.user.profile_image_url_https, `https://twitter.com/${tweet.user.screen_name}`)
+        .setColor(`#${tweet.user.profile_background_color}`)
+
+    // Tweet has media, don't embed it
+    if(tweet.extended_entities && tweet.extended_entities.media) {
+        if(tweet.extended_entities.media[0].type != "photo")  {
+            Utils.sendToChannels(this.client, this.client.config.tweetChannels, tweetLink)
+            return
+        } else
+            embed.setImage(tweet.extended_entities.media[0].media_url_https)
+    }
+
+    let text = tweet.text
+
+    if(tweet.extended_tweet) {
+        text = tweet.extended_tweet.full_text
+        for(const url of tweet.extended_tweet.entities.urls)
+            text = text.replace(url.url, url.expanded_url)
+    } else {
+        for(const url of tweet.entities.urls)
+            text = text.replace(url.url, url.expanded_url)
+    }
+
+    embed.setDescription(text)
+
+    Utils.sendToChannels(this.client, this.client.config.tweetChannels, `<${tweetLink}>`, embed)
 }
 
 exports.shutdown = () => {
