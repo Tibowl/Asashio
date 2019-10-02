28493e9d2b4e24dfb9312abac85508b4733d4a5d
Tibo <Tibowl@users.noreply.github.com>
2019-10-02 14:07:27 +0200

    Prevent ready from triggering twice

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,12 @@
 const Logger = require("log4js").getLogger("ready")
 
+let alreadyLoaded = false
 module.exports = (client) => {
     Logger.info(`In ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`)
+
+    if(alreadyLoaded) return
+    alreadyLoaded = true
+
     client.linkManager.loadLinks(client)
     client.timerManager.init(client)
     client.tweetManager.init(client)
