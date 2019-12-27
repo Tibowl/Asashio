1a6cff1955a02fd284c393f0616758f7b2d555d8
Tibo <Tibowl@users.noreply.github.com>
2019-12-27 17:12:49 +0100

    Ignore capitalization for E dupes

--- src/commands/tools/dupes.js
+++ src/commands/tools/dupes.js
@@ -7,7 +7,7 @@ exports.run = async (message, args) => {
 
     let map, node, difficulty = "H", rank = "S"
     for(let i = args.length - 1; i > 0; i--) {
-        args[i] = args[i].replace(/^E(\d[a-zA-Z0-9]+)$/,"E-$1")
+        args[i] = args[i].replace(/^E(\d[a-zA-Z0-9]+)$/i, "E-$1")
 
         if(args[i].includes("-")) {
             if (!args[i].match(/-\d$/)) {
