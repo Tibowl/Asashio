71f91ea2a14a42d71c4cdf6a7e99176eaeac3398
Tibowl <Tibowl@users.noreply.github.com>
2022-04-10 14:12:03 +0200

    Improve shiplist autocomplete, add stype

--- src/commands/info/shiplist.ts
+++ src/commands/info/shiplist.ts
@@ -24,24 +24,33 @@ export default class ShipList extends Command {
     }
 
     async autocomplete(source: AutocompleteInteraction): Promise<void> {
-        const targetNames = Object.keys(client.data.shiplist)
         const search = source.options.getFocused().toString().split(/\s+/)
 
         const last = search.pop() as string
-        const responses = findFuzzyBestCandidates(targetNames, last, 20).map(value => {
+        const responses = findFuzzyBestCandidates(this.getTargetNames(search), last, 20).map(value => {
             const reply = [...search, value].join(" ")
             return { name: reply, value: reply }
-        }).filter((x) => x.value.split(" ").every((v, i, a) => a.indexOf(v) == i))
+        })
 
         if (responses.length == 1 || responses[0].value == source.options.getFocused().toString()) {
-            await source.respond([...responses, ...findFuzzyBestCandidates(targetNames, "", 20 - responses.length).map(value => {
+            await source.respond([...responses, ...findFuzzyBestCandidates(this.getTargetNames(responses[0].value.split(/ +/)), "", 20 - responses.length).map(value => {
                 const reply = [responses[0].value, value].join(" ")
                 return { name: reply, value: reply }
-            })].filter((x) => x.value.split(" ").every((v, i, a) => a.indexOf(v) == i)))
+            })])
             return
         }
 
         await source.respond(responses)
+    }
+
+    getTargetNames(others: string[]): string[] {
+        const targetNames = Object.keys(client.data.shiplist)
+        if (others.length > 0)
+            targetNames.push(...Object.values(client.data.misc.ShipCodes))
+
+        const criteria = this.mapCriteria(others)
+
+        return targetNames.filter(x => !criteria.includes(x) && this.getMatches([...criteria, x]).length > 0)
     }
 
     async runInteraction(source: CommandInteraction): Promise<SendMessage | undefined> {
@@ -55,16 +64,34 @@ export default class ShipList extends Command {
     }
 
     async run(source: CommandSource, criteriaInput: string[]): Promise<CommandResponse> {
-        const { shiplist } = client.data
-
-        const targetNames = Object.keys(shiplist)
-        const criteria = criteriaInput.map(x => findFuzzy(targetNames, x))
-
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const ships = criteria.reduce((p, c) => p.filter(x => shiplist[c!].includes(x)), shiplist[criteria[0]!])
+        const criteria = this.mapCriteria(criteriaInput)
+        const ships = this.getMatches(criteria)
 
         if (ships.length == 0) return sendMessage(source, `No ships matching criteria: ${criteria.join(", ")}`)
 
         return sendMessage(source, `Ships matching ${criteria.join(", ")}: ${ships.join(", ")}`)
+    }
+
+    private mapCriteria(criteriaInput: string[]): string[] {
+        const targetNames = [...Object.keys(client.data.shiplist), ...Object.values(client.data.misc.ShipCodes)]
+
+        return criteriaInput.map(x => findFuzzy(targetNames, x) as string)
+    }
+
+    private getMatches(criteria: string[]) {
+        const { shiplist, misc, ships } = client.data
+
+
+        function getShips(c: string): string[] {
+            const type = Object.entries(misc.ShipCodes)
+                .find(k => k[1].toUpperCase() === c.toUpperCase())?.[0] ?? 0
+
+            if (type == 0)
+                return shiplist[c]
+            else
+                return Object.values(ships).filter(ship => ship.type == +type).map(s => s.full_name)
+        }
+
+        return criteria.reduce((p, c) => p.filter(x => getShips(c).includes(x)), getShips(criteria[0]))
     }
 }
